41e9f4f1637a9bb788abd0b13e6b24f7565826ce
EstherUduma <estheruduma23@gmail.com>
2024-02-19 16:52:06 +0100

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqalchemy import Column, String, Table, ForeignKey
-from sqalchemy.orm import relationship
-import os
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 from models.place import place_amenity
 
 
