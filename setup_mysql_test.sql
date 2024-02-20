10d5f3565561e93b905e057efd2498e172a3fca6
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-19 20:46:54 -0800

    Task 3 done babee

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- A script that prepares a MySQL server for the project
 
 -- Create the database if it doesn't exist
-CREATE DADTABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create the user and set the password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
