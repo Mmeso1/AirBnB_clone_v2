489362b20c5a8e2a1e429ecbebce3c632fedb2b1
EstherUduma <estheruduma23@gmail.com>
2024-02-21 05:34:00 -0800

    check

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,5 @@
--- A script that prepares a MySQL server for the project
-
--- Create the database if it doesn't exist
+-- Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create the user and set the password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant privileges to the user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- Flush privileges to ensure they take effect
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,5 @@
--- A script that prepares a MySQL server for the project
-
--- Create the database if it doesn't exist
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Create the user and set the password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant privileges to the user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
--- Flush privileges to ensure they take effect
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
