be3ab3fff65060a14b9ac847fb3745c437afa25b
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-21 14:14:08 -0800

    console update

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.place import place_amenity
-
+import os
 
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
@@ -12,5 +12,9 @@ class Amenity(BaseModel, Base):
         name: input name
     """
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity)
+    else:
+        name = ''
