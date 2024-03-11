950de7b9ed2e4ca1b99f3a311f7a5f2a2da9aad8
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2024-03-11 13:41:31 +0100

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,11 +9,22 @@ from fabric.api import run
 
 env.hosts = ["54.90.28.121", "54.237.79.178"]
 
+
 def do_clean(number=0):
-    """Deletes out-of-date archives"""
-    archives = os.listdir('versions/')
-    archives.sort(reverse=True)
-    number = 1 if number == 0 else int(number)
-    for archive in archives[number:]:
-        local(f"rm versions/{archive}")
-        run(f"rm /data/web_static/releases/{archive}")
+    """Deletes out-of-date .tgz archives of web servers
+    """
+
+    number = int(number)
+
+    if number >= 0:
+        with lcd("versions"):
+            _files = gets_out_of_date(number, 'local')
+
+            for _file in _files:
+                local("rm -f {file}".format(file=_file))
+
+        with cd("/data/web_static/releases"):
+            _folders = gets_out_of_date(number, 'remote')
+
+            for _folder in _folders:
+                run("rm -rf {folder}".format(folder=_folder))
