5995779dca3b9b409e833425339de2b709da2098
Mmeso1 <anisiubammesoma8@gmail.com>
2024-03-24 14:04:29 -0700

    task 8 ug

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """ HBNB Flask Server """
 from flask import Flask, render_template
-from models import storage, State
+
+from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-# Teardown app context method
+
+@app.route('/states_list')
+def lists_states():
+    """ List all the states """
+    states = sorted(list(storage.all(State).values()), key=lambda s: s.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def close_storage(exception):
+def app_teardown(exc):
+    """ Teardown flask app context """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all(States).values()
-    sorted_states = sorted(states, key=lambda s: s.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
